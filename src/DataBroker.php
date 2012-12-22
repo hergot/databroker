d0ade153e3cad21ce831c38f5271acd65e0a028d
hergot <milan.hradil@email.cz>
2012-12-22 18:01:56 +0100

    Add comments. Refactor variable name.

--- src/DataBroker.php
+++ src/DataBroker.php
@@ -29,38 +29,41 @@ class DataBroker {
      */
     public function execute($adapterName, array $parameters=array()) {
 	$dataAdapter = $this->dataAdapterLoader->instantiate($adapterName);
+        
+        // Check parameters. Adapter can expect some required parameters 
+        // or some with default values when no present
         $adapterParameters = $dataAdapter->getParameters();
-        foreach ($adapterParameters as $name => $data) {
-            if (is_int($name)) {
-                if (!isset($parameters[$data])) {
-                    $parameters[$data] = null;
+        foreach ($adapterParameters as $name => $specification) {
+            if (is_int($name)) { // parameter without any restriction or specification
+                if (!isset($parameters[$specification])) {
+                    $parameters[$specification] = null; 
                 }
-            } else {
-                if (!is_array($data)) {
-                    throw new \UnexpectedValueException('Parameter definition must be array. Get: ' 
-                            . gettype($data));
+            } else { // parameter with some restriction or specification
+                if (!is_array($specification)) { 
+                    throw new \UnexpectedValueException('Parameter specification must be array. Get: ' 
+                            . gettype($specification));
                 }
                 if (!isset($parameters[$name])) {
-                    if (isset($data['required']) && $data['required'] === true) {
+                    if (isset($specification['required']) && $specification['required'] === true) {
                         throw new DataBrokerException('Missing required parameter "' 
                                 . $name . '" for adapter "' . $adapterName . '"', 
                                 DataBrokerException::MISSING_REQUIRED_PARAMETER);
                     }
-                    if (isset($data['default'])) {
-                        $parameters[$name] = $data['default'];
+                    if (isset($specification['default'])) {
+                        $parameters[$name] = $specification['default'];
                     }
                 }
                 
-                if (isset($data['type']) && gettype($parameters[$name]) !== $data['type']) {
+                if (isset($specification['type']) && gettype($parameters[$name]) !== $specification['type']) {
                     throw new DataBrokerException('Mismatched parameter type.' .
-                            ' Expected "' . $data['type'] . '" get "' . gettype($parameters[$name]) 
+                            ' Expected "' . $specification['type'] . '" get "' . gettype($parameters[$name]) 
                             . '" for parameter "' . $name . '" in adapter "' . $adapterName . '"',
                             DataBrokerException::MISMATCH_PARAMETER_TYPE);
                 }
-                if (isset($data['interface']) && (gettype($parameters[$name]) !== 'object' 
-                        || !($parameters[$name] instanceof $data['interface']))) {
+                if (isset($specification['interface']) && (gettype($parameters[$name]) !== 'object' 
+                        || !($parameters[$name] instanceof $specification['interface']))) {
                     throw new DataBrokerException('Mismatched parameter interface.' .
-                            ' Expected "' . $data['interface'] . '" get "' 
+                            ' Expected "' . $specification['interface'] . '" get "' 
                             . (gettype($parameters[$name]) !== 'object' 
                             ? gettype($parameters[$name]) : get_class($parameters[$name]))
                             . '" for parameter "' . $name . '" in adapter "' . $adapterName . '"',
